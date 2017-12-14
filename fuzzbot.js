3b6253e30d1e0278b70528f6ca037677b1a54c51
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2017-12-14 14:25:58 -0500

    Update fuzzbot.js

--- fuzzbot.js
+++ fuzzbot.js
@@ -40,7 +40,7 @@ client.on('message', (message) => {    // Anything in here happens when someone 
         });
     }
     
-    if (message.content.startsWith("doole")) {
+    if (message.content.startsWith("doole") && Discord.Guild.name = HEYHOE) {
         message.channel.send('srunk and 20???? doole', {
             files: [
                 "./doole.png"
