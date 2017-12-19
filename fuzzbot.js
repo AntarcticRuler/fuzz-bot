7a4341531be0910ebbcccf317f6a8e59529d6247
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2017-12-18 21:24:36 -0500

    Update fuzzbot.js

--- fuzzbot.js
+++ fuzzbot.js
@@ -56,7 +56,7 @@ client.on('message', (message) => {    // Anything in here happens when someone 
         message.channel.send (":eyes:");
     }
     
-    if (message.content.startsWith ("mmm") && message.author.ID == MyID) {
+    if (message.content.startsWith ("mmm")) {
         message.channel.send (":yum:");
     }
     
